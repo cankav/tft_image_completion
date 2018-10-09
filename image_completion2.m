fefdda200b1f2c8c211aa77f3a3342d3bf133109
Can Kavaklioglu <can.kavaklioglu@ryerson.ca>
2018-10-09 16:48:17 -0400

    fixed path name

--- image_completion2.m
+++ image_completion2.m
@@ -1,9 +1,9 @@
 clear all;
 cd /home/can/projeler/tft
 setup_tft
-cd /home/can/projeler/image_completion
+cd /home/can/projeler/tft_image_completion
 
-corrupted_image = imread('/home/can/projeler/image_completion/corrupted_image.png');
+corrupted_image = imread('/home/can/projeler/tft_image_completion/corrupted_image.png');
 I2_corr = double(corrupted_image);
 I2_corr( I2_corr==0 ) = 0.0001;
 
@@ -41,7 +41,7 @@ check_divergence(engine.beta_divergence);
 pred = create_tensor( cellfun( @(index) index.id, X.indices ), 'zeros' );
 gtp(pred, A, B, C);
 
-original_image = imread('/home/can/projeler/image_completion/original.jpg');
+original_image = imread('/home/can/projeler/tft_image_completion/original.jpg');
 
 sqrt( sum(sum( (pred.data - double(original_image)).^2 ) )/prod(size(original_image)))
 
